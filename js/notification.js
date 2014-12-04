225af34f66283c087e16e9b44b1b01af0a70b214
Henk Holtrop <henk_holtrop@live.nl>
2014-12-04 11:38:17 +0100

    Merge branch 'master' of https://github.com/henkholtrop/henkholtrop.github.io
    Conflicts:
    	css/.sass-cache/a3738d6037bd2cee7545d276a63fc12dabc2d36d/_buttons.scssc
    	css/.sass-cache/a3738d6037bd2cee7545d276a63fc12dabc2d36d/_color.scssc
    	css/.sass-cache/a3738d6037bd2cee7545d276a63fc12dabc2d36d/_login.scssc
    
    css/.sass-cache/a3738d6037bd2cee7545d276a63fc12dabc2d36d/_notifications.
    scssc
    	css/.sass-cache/a3738d6037bd2cee7545d276a63fc12dabc2d36d/_popups.scssc
    
    css/.sass-cache/a3738d6037bd2cee7545d276a63fc12dabc2d36d/_typography.scs
    sc
    	css/.sass-cache/a3738d6037bd2cee7545d276a63fc12dabc2d36d/style.scssc
    	css/style.css
    	index.html

--- js/notification.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() {
-  $('.error-notification').animate({"bottom: +=10px"}).animate({"bottom: -10px"}).delay(150).animate({"bottom: +=10px"}).animate({"bottom: -1000px"});
-});
